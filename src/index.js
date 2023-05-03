1b037f9ddb6382c8ea59216b44321c2d07e1bcdf
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-05-03 07:17:24 -0300

    Create List Dynamically filling Input

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import './style.css';
 
 const container = document.querySelector('.to-do-list');
@@ -8,51 +7,54 @@ h1.innerHTML = "Today's To Do";
 container.appendChild(h1);
 
 const form = document.createElement('form');
-form.setAttribute('action', 'javascript:void(0)');
+/* form.setAttribute('action', 'javascript:void(0)'); */
 const input = document.createElement('input');
-input.type = "text";
-input.name = "description";
-input.placeholder = "Add to your list..."
+input.type = 'text';
+input.name = 'description';
+input.placeholder = 'Add to your list...';
 form.appendChild(input);
 container.appendChild(form);
 
-const Task1 = {
-  description: "Wash dishesr",
-  completed: false,
-  index: 0,
+const arryOfTasks = [];
+
+const Task = {
+  create(description, index) {
+    const task = Object.create(this);
+    task.description = description;
+    task.completed = false;
+    task.index = index;
+    return task;
+  },
 };
 
-const Task2 = {
-  description: "Fix car",
-  completed: false,
-  index: 1,
-};
-
-const Task3 = {
-  description: "Practice Instrument",
-  completed: false,
-  index: 2,
-};
-const arryOfTasks = [Task1, Task2, Task3];
+input.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter' && input.value !== '') {
+    e.preventDefault();
+    const task = Task.create(input.value, arryOfTasks.length);
+    arryOfTasks.push(task);
+    input.value = '';
+    localStorage.setItem('tasks', JSON.stringify(arryOfTasks));
+  }
+});
 
 const ul = document.createElement('ul');
 
-for (let i = 0; i < arryOfTasks.length; i += 1) {
-  const li = document.createElement('li')
-  const checkBox = document.createElement('input');
-  checkBox.type = 'checkbox';
-  li.appendChild(checkBox);
-  checkBox.checked = arryOfTasks[i].completed;
-  const p = document.createElement('p');
-  p.innerHTML = arryOfTasks[i].description;
-  li.appendChild(p);
-  ul.appendChild(li);
+if (localStorage.length !== 0) {
+  document.addEventListener('DOMContentLoaded', () => {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks'));
+    for (let i = 0; i < storedTasks.length; i += 1) {
+      const li = document.createElement('li');
+      const checkBox = document.createElement('input');
+      checkBox.type = 'checkbox';
+      li.appendChild(checkBox);
+      checkBox.checked = storedTasks[i].completed;
+      const p = document.createElement('p');
+      p.innerHTML = storedTasks[i].description;
+      storedTasks[i].index = i;
+      li.appendChild(p);
+      ul.appendChild(li);
+      arryOfTasks.push(storedTasks[i]);
+    }
+    container.appendChild(ul);
+  });
 }
-container.appendChild(ul);
-/*
-let Task = {
-}
-const task = new Task();
-
-const arryOfTasks = [];
- */
