c7364c201a1082a4f1c01466d7adab5851f62c50
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-05-03 06:41:11 -0300

    Add HTML dynamically

--- src/index.js
+++ src/index.js
@@ -1,10 +1,58 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  return element;
-}
+const container = document.querySelector('.to-do-list');
 
-document.body.appendChild(component());
+const h1 = document.createElement('h1');
+h1.innerHTML = "Today's To Do";
+container.appendChild(h1);
+
+const form = document.createElement('form');
+form.setAttribute('action', 'javascript:void(0)');
+const input = document.createElement('input');
+input.type = "text";
+input.name = "description";
+input.placeholder = "Add to your list..."
+form.appendChild(input);
+container.appendChild(form);
+
+const Task1 = {
+  description: "Wash dishesr",
+  completed: false,
+  index: 0,
+};
+
+const Task2 = {
+  description: "Fix car",
+  completed: false,
+  index: 1,
+};
+
+const Task3 = {
+  description: "Practice Instrument",
+  completed: false,
+  index: 2,
+};
+const arryOfTasks = [Task1, Task2, Task3];
+
+const ul = document.createElement('ul');
+
+for (let i = 0; i < arryOfTasks.length; i += 1) {
+  const li = document.createElement('li')
+  const checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
+  li.appendChild(checkBox);
+  checkBox.checked = arryOfTasks[i].completed;
+  const p = document.createElement('p');
+  p.innerHTML = arryOfTasks[i].description;
+  li.appendChild(p);
+  ul.appendChild(li);
+}
+container.appendChild(ul);
+/*
+let Task = {
+}
+const task = new Task();
+
+const arryOfTasks = [];
+ */
